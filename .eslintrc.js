399024efd8596b8ded25cf32acffffbf23e854a3
Maciej Szostak <maciek.szostak55@gmail.com>
2023-09-17 09:26:29 +0200

    Configure ESLint, husky and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
+  extends: ['next'],
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+}
