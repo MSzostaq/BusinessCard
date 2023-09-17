af2c2ae68d74ad104d1d1d07daf05de1f99aa69c
Maciej Szostak <maciek.szostak55@gmail.com>
2023-09-17 18:09:17 +0200

    WiP Content layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,46 +5,48 @@ export default function Home() {
   return (
     <>
       <title>Frontend Developer - Maciej Szostak</title>
-      <main className="flex items-center justify-center h-screen p-12">
-        <div className="flex ">
-          <div className="rounded-xl w-1/3 h-1/2">
-            <Image
-              src={'/avatar.jpeg'}
-              alt="avatar"
-              layout="responsive"
-              width={100}
-              height={100}
-              className="rounded-full"
-            />
-          </div>
-          <div className="">
-            <div>
-              <h1>Maciej Szostak</h1>
-              <p>Frontend Developer</p>
+      <main className="px-12 h-screen">
+        <div className="flex flex-col h-full w-full">
+          <div className="flex w-full items-center ">
+            <div className="w-1/3 h-1/3">
+              <Image
+                src={'/avatar.jpeg'}
+                alt="avatar"
+                layout="responsive"
+                width={100}
+                height={100}
+                className="rounded-full"
+              />
             </div>
-            <div>
-              <p>Technologies that i have experienice with:</p>
-              <li className="flex items-center justify-start">
-                <Icon name="react" className="w-5 h-5" />
-                <p>React/NextJS</p>
-              </li>
-              <li className="flex items-center justify-start">
-                <Icon name="typescript" className="w-5 h-5" />
-                <p>TypeScript/JavaScript</p>
-              </li>
-              <li className="flex items-center justify-start">
-                <Icon name="html" className="w-5 h-5" />
-                <p>HTML</p>
-              </li>
-              <li className="flex items-center justify-start">
-                <Icon name="css" className="w-5 h-5" />
-                <p>CSS</p>
-              </li>
+            <div className="">
+              <div>
+                <h1 className="font-bold text-4xl">Maciej Szostak</h1>
+                <h2 className="text-2xl font-medium">Frontend Developer</h2>
+              </div>
+              <div>
+                <p>Technologies that i have experienice with:</p>
+                <li className="flex items-center justify-start">
+                  <Icon name="react" className="w-5 h-5" />
+                  <p>React/NextJS</p>
+                </li>
+                <li className="flex items-center justify-start">
+                  <Icon name="typescript" className="w-5 h-5" />
+                  <p>TypeScript/JavaScript</p>
+                </li>
+                <li className="flex items-center justify-start">
+                  <Icon name="html" className="w-5 h-5" />
+                  <p>HTML</p>
+                </li>
+                <li className="flex items-center justify-start">
+                  <Icon name="css" className="w-5 h-5" />
+                  <p>CSS</p>
+                </li>
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <p>2023 Maciej Szostak</p>
+          <div>
+            <p>2023 Maciej Szostak</p>
+          </div>
         </div>
       </main>
     </>
