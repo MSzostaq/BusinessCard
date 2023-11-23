4dd2fa6eb8810ebb1b3e7d0b85662928a7a436bc
Maciej Szostak <maciek.szostak55@gmail.com>
2023-11-23 09:39:39 +0100

    Update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
   return (
     <>
       <title>Frontend Developer - Maciej Szostak</title>
-      <main className="overflow-x-hidden">
+      <main>
         <div className="flex items-cen flex-col justify-center h-full w-full p-2 sm:px-6 lg:p-12 xl:px-20 2xl:px-32">
-          <div className="flex flex-col lg:flex-row items-start lg:space-x-5 xl:space-x-8 w-full">
+          <div className="flex flex-col lg:flex-row items-start lg:space-x-5 xl:space-x-6 w-full">
             <div className="w-full lg:w-2/5 flex lg:justify-end">
-              <div className="rounded-full w-40 h-40 md:w-44 md:h-44 mx-2 mt-4 lg:mx-0 md:mt-0 lg:mt-3">
+              <div className="w-40 h-40 md:w-44 md:h-44 mx-2 mt-4 lg:mx-0 md:mt-0 lg:mt-2">
                 <Image
                   src={`/avatar.png`}
                   alt="avatar"
@@ -24,14 +24,14 @@ export default function Home() {
                 />
               </div>
             </div>
-            <div className="w-full lg:w-3/5 justify-start lg:mt-8 px-2">
-              <h1 className="font-extrabold text-4xl sm:text-5xl md:text-6xl mt-4 xl:text-7xl text-gray-800 tracking-wide">
+            <div className="w-full lg:w-3/5 justify-start px-2">
+              <h1 className="font-extrabold text-3xl sm:text-4xl md:text-5xl mt-4 xl:text-6xl text-gray-800 tracking-wide">
                 Maciej Szostak
               </h1>
-              <h2 className="tracking-wide text-xl sm:text-2xl md:text-3xl xl:text-4xl font-bold text-gray-700">
+              <h2 className="tracking-wide text-xl sm:text-xl md:text-2xl xl:text-3xl font-bold text-gray-700">
                 Frontend Developer
               </h2>
-              <div className="flex space-x-2 mb-1 mt-2 md:mt-3 xl:mt-4">
+              <div className="flex space-x-2 mb-1 mt-2">
                 <SocialMediaLink
                   href="https://github.com/MSzostaq"
                   icon="github"
@@ -41,7 +41,7 @@ export default function Home() {
                   icon="linkedin"
                 />
               </div>
-              <ol className="mt-6 lg:mt-8 2xl:mt-8">
+              <ol className="mt-6 2xl:mt-7">
                 <li className="mb-1 lg:mb-2 text-lg sm:text-xl md:text-2xl xl:text-3xl font-bold tracking-wide text-gray-800">
                   My tech stack
                 </li>
--- src/components/ResumeButton.tsx
+++ src/components/ResumeButton.tsx
@@ -4,11 +4,11 @@ import { Icon } from '@/components/Icon'
 
 export function ResumeButton() {
   return (
-    <li className="flex items-center justify-start w-full mb-6 ">
+    <li className="flex items-center justify-start w-full mb-6 md:mb-0">
       <Link
         href="/docs/cv.pdf"
         target="_blank"
-        className="bg-green-600 rounded-lg px-4 py-3 mt-4 md:mt-4 flex items-center justify-center space-x-2"
+        className="bg-green-600 rounded-lg px-4 py-3 mt-4 md:mt-3 flex items-center justify-center space-x-2"
       >
         <Icon name="cv" className="fill-white w-5 h-5" />
         <p className="text-white font-bold text-lg">Resume</p>
